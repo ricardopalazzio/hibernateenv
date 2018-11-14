76d50bae6ebe1e37b82cf75934e2a047b4b3fc90
tux <tux@localhost.localdomain>
2018-11-14 21:39:00 -0200

    teste webhook1

--- src/main/java/ninja/tuxtech/envers/ClienteRepository.java
+++ src/main/java/ninja/tuxtech/envers/ClienteRepository.java
@@ -2,6 +2,7 @@ package ninja.tuxtech.envers;
 
 import ninja.tuxtech.envers.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
--- src/main/java/ninja/tuxtech/envers/model/UserRevEntity.java
+++ src/main/java/ninja/tuxtech/envers/model/UserRevEntity.java
@@ -1,17 +1,19 @@
 package ninja.tuxtech.envers.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+// import javax.xml.bind.annotation.XmlRootElement;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import org.hibernate.envers.DefaultRevisionEntity;
 import org.hibernate.envers.RevisionEntity;
 
-import javax.persistence.Entity;
-import javax.xml.bind.annotation.XmlRootElement;
-import java.io.Serializable;
-
 @Entity
 @RevisionEntity(UserRevisionListener.class)
-@XmlRootElement
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+// @XmlRootElement
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class UserRevEntity extends DefaultRevisionEntity implements Serializable {
     private String username;
     private String ip;
